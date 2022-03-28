e1f92abb5a0871d401611a8bbf17d48107fcfb9d
hj <2277075501@qq.com>
2022-03-28 22:34:27 +0800

    79-88

--- packHouse/algorithm/offer_jianzhi/83.cpp
+++ packHouse/algorithm/offer_jianzhi/83.cpp
@@ -1,14 +1,40 @@
-// Copyright 2022 icf
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+#include <bits/stdc++.h>
+using namespace std;
 
+
+vector<int> nums = {1,2,3};
+vector<vector<int>> res;
+vector<int> path;
+
+int n = nums.size();
+vector<int> visited(n,false);
+
+void dfs(vector<int>&path){
+    if(path.size()==n){
+        res.push_back(path);
+        return ;
+    }
+    
+    for(int i=0;i<n;i++){
+        if(visited[i]==false){
+            visited[i] = true;
+            path.push_back(nums[i]);
+            dfs(path);
+            path.pop_back();
+            visited[i] = false;
+        }
+    }
+    
+}
+
+
+int main(){
+    dfs(path);
+    
+    for(auto c:res){
+        for(auto d:c){
+            cout<<d<<" ";
+        }
+        cout<<endl;
+    }
+}
