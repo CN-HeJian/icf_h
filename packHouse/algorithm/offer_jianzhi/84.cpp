21535d44a8627e25ec847d8870e7666488c8e4f8
hj <2277075501@qq.com>
2022-03-28 22:36:29 +0800

    79-88

--- packHouse/algorithm/offer_jianzhi/84.cpp
+++ packHouse/algorithm/offer_jianzhi/84.cpp
@@ -1,14 +1,42 @@
-// Copyright 2022 icf
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
+vector<int> nums  = {1,1,1};
+int n = nums.size();
+
+vector<int> visited(n,false);
+
+vector<vector<int>> res;
+vector<int> path;
+
+void dfs(vector<int>& path){
+    if(path.size()==n){
+        res.push_back(path);
+        return ;
+    }
+    for(int i=0;i<n;i++){
+        if(i>0 &&nums[i]==nums[i-1]&&visited[i-1]==false){
+            continue;
+        }
+        if(visited[i]==false){
+            path.push_back(nums[i]);
+            visited[i] = true;
+            dfs(path);
+            path.pop_back();
+            visited[i] = false;
+        }
+    }
+}
+
+
+int main(){
+    sort(nums.begin(),nums.end());
+    dfs(path);
+    for(auto c:res){
+        for(auto d:c){
+            cout<<d<<" ";
+        }
+        cout<<endl;
+    }
+}
